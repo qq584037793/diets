f3e44d83adf8ceab68f5cf6f258a11721f33d701
gao <qq584496895@gmail.com>
2020-07-07 13:13:42 +0000

    コントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'rakudiet#home'
+  get 'rakudiet/about'
+  get 'rakudiet/terms'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
